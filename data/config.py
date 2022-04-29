1ee8c49a52f2b8c139b82dd9bea6157d3779ce16
Asian <theavsv@gmail.com>
2022-04-29 14:29:49 +0300

    adaptive to heroku

--- data/config.py
+++ data/config.py
@@ -1,9 +1,11 @@
 from pathlib import Path
+import os
 
-BOT_TOKEN = ''
-BASE_URL = 'https://example.com'  # Webhook domain
-WEBHOOK_PATH = f'/tg/webhooks/bot/{BOT_TOKEN}'
-WEBHOOK_URL = f'{BASE_URL}{WEBHOOK_PATH}'
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+TOKEN = os.getenv('TOKEN')
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 LOGS_BASE_PATH = str(Path(__file__).parent.parent / 'logs')
 
